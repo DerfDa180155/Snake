3e3d59bd0d9c0b718b757225ab96dec6666d6e7d
DerfDa180155 <derflinger.david@gmail.com>
2026-03-19 23:50:54 +0100

    added code setup

---
+++ src/Main.py
@@ -0,0 +1,45 @@
+import pygame
+
+
+class main:
+    def __init__(self):
+        pygame.init()
+        pygame.display.init()
+
+        pygame.display.gl_set_attribute(pygame.GL_ACCELERATED_VISUAL, 0)
+        pygame.display.gl_set_attribute(pygame.GL_DOUBLEBUFFER, 1)
+
+        self.windowWidth = 1500
+        self.windowHeight = 1500
+
+        self.screen = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.GL_DOUBLEBUFFER)
+        pygame.display.set_caption("Snake by David Derflinger")
+
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.run()
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: # Quit the Game
+                    self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE: # Quit the Game
+                        self.running = False
+
+            self.windowWidth = self.screen.get_width()
+            self.windowHeight = self.screen.get_height()
+
+            self.screen.fill((50, 50, 50))
+
+
+
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
