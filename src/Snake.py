91545ce6616e60c9d4c12af0da47e95cb9d596cc
DerfDa180155 <derflinger.david@gmail.com>
2026-03-22 23:54:29 +0100

    added empty map generator

--- src/Snake.py
+++ src/Snake.py
@@ -4,10 +4,18 @@ class Snake:
     def __init__(self):
         self.map = []
 
+        self.sizeX = 20
+        self.sizeY = 20
+
         self.generateEmptyBoard()
 
     def generateEmptyBoard(self):
-        pass
+        self.map = []
+        for i in range(self.sizeY):
+            temp = []
+            for j in range(self.sizeX):
+                temp.append("")
+            self.map.append(temp)
 
     def movePlayer(self):
         pass
