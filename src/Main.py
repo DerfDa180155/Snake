5d6f508806c0c0e24bab41279b273f996c3090c2
DerfDa180155 <derflinger.david@gmail.com>
2026-04-27 23:56:43 +0200

    updated code

--- src/Main.py
+++ src/Main.py
@@ -74,7 +74,7 @@ class main:
                 pygame.draw.rect(self.screen, color, (startX + width/self.snake.sizeX*x, startY + height/self.snake.sizeY*y, (width/self.snake.sizeX)-gapSize, (height/self.snake.sizeY)-gapSize))
 
     def drawGameOver(self, posX, posY, sizeX, sizeY):
-        pygame.draw.rect(self.screen, (10, 10, 10), (self.windowWidth/2-500, self.windowHeight/2-400, 1000, 800))
+        pygame.draw.rect(self.screen, (10, 10, 10), (posX, posY, sizeX, sizeY))
         text = "GameOver"
         textSize = 20
 
