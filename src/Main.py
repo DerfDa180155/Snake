a634eb38210ed1c76c8700cb18697499bed7aed1
DerfDa180155 <derflinger.david@gmail.com>
2026-05-01 23:42:48 +0200

    updated code

--- src/Main.py
+++ src/Main.py
@@ -85,6 +85,15 @@ class main:
         newRect.centery = posY + sizeY/4
         self.screen.blit(text, newRect)
 
+        text = "Score: " + "1"
+        textSize = 30
+        font = pygame.font.Font(pygame.font.get_default_font(), textSize)
+        text = font.render(text, True, (255, 255, 255))
+        newRect = text.get_rect()
+        newRect.centerx = posX + sizeX / 2
+        newRect.centery = posY + sizeY / 2
+        self.screen.blit(text, newRect)
+
 
 if __name__ == "__main__":
     main()
