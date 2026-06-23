f62ea6c624c7c4ad63a42680d435a66024380d9b
DerfDa180155 <derflinger.david@gmail.com>
2026-06-23 23:50:30 +0200

    updated code

--- src/Button.py
+++ src/Button.py
@@ -17,6 +17,15 @@ class Button:
     def draw(self):
         pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.sizeX, self.sizeY))
 
+        textSize = 50
+        font = pygame.font.Font(pygame.font.get_default_font(), textSize)
+
+        text = font.render(self.onClick, True, (0, 150, 120))
+        newRect = text.get_rect()
+        newRect.centerx = self.x + self.sizeX/2
+        newRect.centery = self.y + self.sizeY/2
+        self.screen.blit(text, newRect)
+
     def clicked(self, mx, my, mouseClick):
         if self.hover(mx, my) and mouseClick[0]:
             self.isleftClicked = True
